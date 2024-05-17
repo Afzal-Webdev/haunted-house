938eb111f0894dd4da9646b09ac8d1cd163803fc
afzal <afzal.appelit@nextbridge.com>
2024-05-17 20:50:08 +0500

    Github Pages Deployment Settings and Path Updates

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 export default {
     root: 'src/',
     publicDir: '../static/',
-    base: './',
-    server:
-    {
+    base: '/haunted-house/',  // Set to your repository name for GitHub Pages
+    // For Working locally
+    // base: './',  
+    server: {
         host: true, // Open to local network and display URL
         open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
     },
-    build:
-    {
+    build: {
         outDir: '../dist', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
         sourcemap: true // Add sourcemap
